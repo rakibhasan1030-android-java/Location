81186377267f1a194062d78b723050183df423f3
rakib <rakibhasan1030@gmail.com>
2020-03-19 08:01:59 +0600

    geoCoder...

--- app/src/main/java/com/rakib/location/MainActivity.java
+++ app/src/main/java/com/rakib/location/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,12 +19,20 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient client;
     private LocationRequest request;
     private LocationCallback callback;
-    private TextView latTV, lonTV;
+    private TextView latTV, lonTV, addressTV;
+    private Geocoder geocoder; // geocoder will give you physical address
+    private List<Address> addresses = new ArrayList<>();
+    private String _address = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         latTV = findViewById(R.id.latTV);
         lonTV = findViewById(R.id.lonTV);
+        addressTV = findViewById(R.id.addressTV);
 
+        geocoder = new Geocoder(this, Locale.getDefault());
 
         client = LocationServices.getFusedLocationProviderClient(this);
 
@@ -44,10 +57,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
+
                 //the location will come with an array, so we've to find it
                 for(Location location : locationResult.getLocations()){
-                    latTV.setText(String.valueOf(location.getLatitude()));
-                    lonTV.setText(String.valueOf(location.getLongitude()));
+
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    latTV.setText(String.valueOf(latitude));
+                    lonTV.setText(String.valueOf(longitude));
+
+                    try {
+                        addresses = geocoder.getFromLocation(latitude, longitude, 5);
+                        _address = addresses.get(0).getAddressLine(0) + "\n"+
+                                addresses.get(0).getLocality()+"\n"+
+                                addresses.get(0).getCountryName()+"("+
+                                addresses.get(0).getCountryCode()+")";
+
+                        Toast.makeText(getApplicationContext(), "_address", Toast.LENGTH_LONG).show();
+
+                        addressTV.setText("_address");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e("MYAPP", "exception: " + e.getMessage());
+
+                    }
+
                 }
             }
         };
